ab1a0e8ae75b8a3bc3f19d4c126b7e3d1325440e
AlexAutoTesting <zaehor@gmail.com>
2023-08-25 15:51:30 +0300

    3. GC. Out Of Mem. Тестовое задание. [#504790]

--- src/main/java/ru/job4j/articles/Application.java
+++ src/main/java/ru/job4j/articles/Application.java
@@ -12,20 +12,17 @@ import java.util.Properties;
 
 public class Application {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class.getSimpleName());
-
-    public static final int TARGET_COUNT = 1_000_000;
-
     public static void main(String[] args) {
         var properties = loadProperties();
         var wordStore = new WordStore(properties);
         var articleStore = new ArticleStore(properties);
         var articleGenerator = new RandomArticleGenerator();
         var articleService = new SimpleArticleService(articleGenerator);
-        articleService.generate(wordStore, TARGET_COUNT, articleStore);
+        articleService.generate(wordStore, 1_000_000, articleStore);
     }
 
     private static Properties loadProperties() {
+        Logger LOGGER = LoggerFactory.getLogger(Application.class.getSimpleName());
         LOGGER.info("Загрузка настроек приложения");
         var properties = new Properties();
         try (InputStream in = Application.class.getClassLoader().getResourceAsStream("application.properties")) {
@@ -36,5 +33,4 @@ public class Application {
         }
         return properties;
     }
-
 }
